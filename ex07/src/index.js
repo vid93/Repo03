9f9c5a59d68b780d03e68acbd2761f81c42077aa
Vid <vidmiljanovic@gmail.com>
2021-09-15 15:50:26 +0200

    Camel case vars

---
+++ ex07/src/index.js
@@ -0,0 +1,22 @@
+function main(){
+    //Only change code below this line
+    //Variable declarations
+    var stupidlyCapVar;
+    var properCamelCase;
+    var titleCaseOver;
+
+    //Variable assignments
+    stupidlyCapVar = 10;
+    properCamelCase = "A String";
+    titleCaseOver = 9000;
+
+    //Only change code above this line
+    return {
+        stupidlyCapVar,
+        properCamelCase,
+        titleCaseOver
+    }
+}
+
+console.log(main());
+module.exports = main;
