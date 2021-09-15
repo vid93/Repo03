c61f8ed68973e41bf7dd53f5282ded2d40f630eb
Vid <vidmiljanovic@gmail.com>
2021-09-15 16:26:30 +0200

    Math funcs

---
+++ ex09/src/index.js
@@ -0,0 +1,12 @@
+function main(numOne, numTwo){
+    //Only change code below this line
+    difference = numOne - numTwo;
+    //Only change code above this line
+    return difference;
+}
+
+console.log(main(15, 8)); // change this line
+console.log(main(25, 8)); // change this line
+console.log(main(425, 120)); // change this line
+
+module.exports = main;
---
+++ ex10/src/index.js
@@ -0,0 +1,12 @@
+function main(numOne, numTwo){
+    //Only change code below this line
+    product = numOne * numTwo;
+    //Only change code above this line
+    return product;
+}
+
+console.log(main(15, 3)); // change this line
+console.log(main(25, 8)); // change this line
+console.log(main(425, 10)); // change this line
+
+module.exports = main;
