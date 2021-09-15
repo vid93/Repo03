b4901b5bc7aa8925ae5f838aec543804a333a72b
Vid <vidmiljanovic@gmail.com>
2021-09-15 16:12:06 +0200

    Sum of 2 nums

---
+++ ex08/src/index.js
@@ -0,0 +1,12 @@
+function main(numOne, numTwo){
+    //Only change code below this line
+    sum = numOne + numTwo;
+    //Only change code above this line
+    return sum;
+}
+
+console.log(main(15, 8)); // change this line
+console.log(main(5, 28)); // change this line
+console.log(main(150, 425)); // change this line
+
+module.exports = main;
