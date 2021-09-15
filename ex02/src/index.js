93d7fadcbc88045c634c799196cd71e519a0c1f2
Vid <vidmiljanovic@gmail.com>
2021-09-15 14:55:11 +0200

    Added comment ex

---
+++ ex02/src/index.js
@@ -0,0 +1,17 @@
+function main() {
+    // Only change code below this line
+    
+    //This is single line comment
+
+    /*This is
+    a multi-line
+    comment
+     */
+
+    //Only change code above this line
+
+}
+
+console.log(main());
+
+module.exports = main;
